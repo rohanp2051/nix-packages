36cb2173b903bedc1be19e5f865331b0ed7dede4
rohanp2051 <93092652+rohanp2051@users.noreply.github.com>
2026-04-10 06:31:52 +0000

    chore(wispr-flow): update to 1.4.822

--- pkgs/wispr-flow/package.nix
+++ pkgs/wispr-flow/package.nix
@@ -9,18 +9,18 @@
 # Latest version: https://dl.wisprflow.com/wispr-flow/darwin/arm64/RELEASES.json
 stdenvNoCC.mkDerivation {
   pname = "wispr-flow";
-  version = "1.4.642";
+  version = "1.4.822";
 
   src = fetchurl {
-    url = "https://dl.wisprflow.com/wispr-flow/darwin/arm64/dmgs/Flow-v1.4.642.dmg";
-    hash = "sha256-1G9wkdaT7iyHGW0uo5YOqXMhL7GKcKOdS93AMw/4Blk=";
+    url = "https://dl.wisprflow.com/wispr-flow/darwin/arm64/dmgs/Flow-v1.4.822.dmg";
+    hash = "sha256-GsFb11vJ+asTOBCGS0OML0mNtsPPzQl6NU4aQKA8xWQ=";
   };
 
   nativeBuildInputs = [
     _7zz
     asar
   ];
-  sourceRoot = "Flow-v1.4.642/Wispr Flow.app";
+  sourceRoot = "Flow-v1.4.822/Wispr Flow.app";
 
   dontPatch = true;
   dontConfigure = true;
